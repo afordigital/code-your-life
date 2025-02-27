7193d75c500b8f51994052cfcdba560dc5388956
Sara Montagud <sara@know.ee>
2025-02-27 21:02:43 +0100

    feat: add supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { createClient, type Session } from "@supabase/supabase-js";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@supabase/auth-ui-react";
+import "./App.css";
+
+const projectUrl: string = import.meta.env.VITE_PROJECT_URL;
+const apiKey: string = import.meta.env.VITE_API_KEY;
+
+export const supabase = createClient(projectUrl, apiKey);
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [session, setSession] = useState<Session | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			setSession(session);
+		});
+
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session);
+		});
+
+		return () => subscription.unsubscribe();
+	}, []);
+
+	const signInWithGoogle = async () => {
+		const { error } = await supabase.auth.signInWithOAuth({
+			provider: "google",
+		});
+
+		if (error) console.error("Error al iniciar sesión:", error.message);
+	};
+
+	if (!session) {
+		return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+	}
+	return (
+		<div>
+			<p>Logged in!</p>
+			<button type="button" onClick={signInWithGoogle}>
+				Iniciar sesión con Google
+			</button>
+		</div>
+	);
 }
 
-export default App
+export default App;
