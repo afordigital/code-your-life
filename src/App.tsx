4892d4c5fde88ce4cc9d1ec76d2a320ebe5c0d9a
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-03-04 22:21:30 +0100

    feat: try to make a swappable grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { Ref, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import { apiClient } from "./utils/api";
 import { useAuth } from "./hooks/useAuth";
 import {
-	useCreateLifeHistory,
-	useDeleteLifeHistory,
-	useGetUserLifeHistories,
+  useCreateLifeHistory,
+  useDeleteLifeHistory,
+  useGetUserLifeHistories,
 } from "./services/lifeHistory";
 import { useGetCurrentUser } from "./services/user";
 import type { CurrentUser } from "./types";
@@ -14,152 +14,190 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 // import { Modal } from "./components/Modal";
 import { TitleSelector } from "./components/TitleSelector";
+import { createSwapy, Swapy, utils } from "swapy";
 
 export type LifeUnit = "life" | "year" | "month";
 export type TimeUnit = "years" | "months" | "weeks";
 
 function App() {
-	// const [text, setText] = useState("");
-	const {
-	 mutateAsync: getUserAsync,
-	 isPending: getUserIsPending,
-	} = useGetCurrentUser();
-	const { signInWithGoogle, signOut } = useAuth();
-	const deleteLifeHistory = useDeleteLifeHistory();
-	const createLifeHistory = useCreateLifeHistory();
-	// const [isOpen, setIsOpen] = useState<boolean>(false);
-	const getUserLifeHistories = useGetUserLifeHistories();
-	const [currentUser, setCurrentUser] = useState<CurrentUser | null>(null);
-	const [lifeUnit, setLifeUnit] = useState<LifeUnit>("life");
-	const [timeUnit, setTimeUnit] = useState<TimeUnit>("years");
+  // const [text, setText] = useState("");
+  const { mutateAsync: getUserAsync, isPending: getUserIsPending } =
+    useGetCurrentUser();
+  const { signInWithGoogle, signOut } = useAuth();
+  const deleteLifeHistory = useDeleteLifeHistory();
+  const createLifeHistory = useCreateLifeHistory();
+  // const [isOpen, setIsOpen] = useState<boolean>(false);
+  const getUserLifeHistories = useGetUserLifeHistories();
+  const [currentUser, setCurrentUser] = useState<CurrentUser | null>(null);
+  const [lifeUnit, setLifeUnit] = useState<LifeUnit>("life");
+  const [timeUnit, setTimeUnit] = useState<TimeUnit>("years");
 
-	// const handleCreateLifeHistory = async () => {
-	// 	if (createLifeHistory.isPending) return;
+  const generateGrid = (lifeUnit: LifeUnit, timeUnit: TimeUnit) => {
+    let cells: number[] = [];
 
-	// 	if (!currentUser) {
-	// 		console.error("User is not authenticated");
-	// 		return;
-	// 	}
+    if (lifeUnit === "life") {
+      cells = Array.from({ length: 100 }, (_, i) => i + 1); // 1 a 100 años
+    } else if (lifeUnit === "year") {
+      cells =
+        timeUnit === "months"
+          ? Array.from({ length: 12 }, (_, i) => i + 1)
+          : Array.from({ length: 52 }, (_, i) => i + 1); // Meses o semanas
+    } else if (lifeUnit === "month") {
+      cells = Array.from({ length: 4 }, (_, i) => i + 1); // 4 semanas
+    }
 
-	// 	const newLifeHistory: InsertLifeHistory = {
-	// 		event_text: text,
-	// 		user_id: currentUser.id,
-	// 		// rest of the fields:
-	// 		// event_date,
-	// 		// event_image,
-	// 	};
+    return cells;
+  };
 
-	// 	createLifeHistory.mutate(newLifeHistory);
-	// 	setText("");
-	// };
+  let cells = generateGrid(lifeUnit, timeUnit);
 
-	useEffect(() => {
-		const {
-			data: { subscription },
-		} = apiClient.auth.onAuthStateChange((_event, session) => {
-			if (session?.user.id) {
-				try {
-					getUserAsync(session.user.id).then((user) => {
-						if (user) setCurrentUser(user);
-					});
-				} catch (error) {
-					console.error(error);
-				}
-			}
-		});
+  const [slotItemMap, setSlotItemMap] = useState(
+    utils.initSlotItemMap(cells, "cellsId")
+  );
+  const slottedItems = useMemo(
+    () => utils.toSlottedItems(cells, "cellsId", slotItemMap),
+    [cells, slotItemMap]
+  );
 
-		return () => {
-			subscription.unsubscribe();
-		};
-	}, [getUserAsync]);
+  useEffect(
+    () =>
+      utils.dynamicSwapy(
+        swapyRef.current,
+        cells,
+        "cellsId",
+        slotItemMap,
+        setSlotItemMap
+      ),
+    [cells]
+  );
 
-	if (!currentUser) {
-		return (
-			<button type="button" onClick={signInWithGoogle}>
-				{getUserIsPending ? "Loading..." : "Iniciar sesión con Google"}
-			</button>
-		);
-	}
+  // const handleCreateLifeHistory = async () => {
+  // 	if (createLifeHistory.isPending) return;
 
-	const generateGrid = () => {
-		let cells: number[] = [];
+  // 	if (!currentUser) {
+  // 		console.error("User is not authenticated");
+  // 		return;
+  // 	}
 
-		if (lifeUnit === "life") {
-			cells = Array.from({ length: 100 }, (_, i) => i + 1); // 1 a 100 años
-		} else if (lifeUnit === "year") {
-			cells =
-				timeUnit === "months"
-					? Array.from({ length: 12 }, (_, i) => i + 1)
-					: Array.from({ length: 52 }, (_, i) => i + 1); // Meses o semanas
-		} else if (lifeUnit === "month") {
-			cells = Array.from({ length: 4 }, (_, i) => i + 1); // 4 semanas
-		}
+  // 	const newLifeHistory: InsertLifeHistory = {
+  // 		event_text: text,
+  // 		user_id: currentUser.id,
+  // 		// rest of the fields:
+  // 		// event_date,
+  // 		// event_image,
+  // 	};
 
-		return (
-			<div className="grid grid-cols-10 gap-2 mt-4">
-				{cells.map((cell) => (
-					<div
-						key={cell}
-						className="border p-2 text-center rounded bg-gray-100"
-					>
-						{cell}
-					</div>
-				))}
-			</div>
-		);
-	};
+  // 	createLifeHistory.mutate(newLifeHistory);
+  // 	setText("");
+  // };
+  const swapyRef = useRef<Swapy | null>(null);
+  const containerRef = useRef<HTMLElement | null>(null);
 
-	return (
-		<main className="flex flex-col items-center justify-center h-screen min-h-full gap-8 py-20">
-			<Header currentUser={currentUser} />
+  useEffect(() => {
+    swapyRef.current = createSwapy(containerRef.current, {
+      manualSwap: true,
+    });
 
-			<section className="max-w-6xl w-full mx-auto flex flex-col gap-4 flex-1 items-center">
-				{createLifeHistory.isPending ? (
-					<p>Loading...</p>
-				) : createLifeHistory.isError ? (
-					<p>Error: {createLifeHistory.error.message}</p>
-				) : null}
+    return () => {
+      swapyRef.current?.destroy();
+    };
+  }, []);
 
-				{deleteLifeHistory.isPending ? (
-					<p>Loading...</p>
-				) : deleteLifeHistory.isError ? (
-					<p>Error: {deleteLifeHistory.error.message}</p>
-				) : null}
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = apiClient.auth.onAuthStateChange((_event, session) => {
+      if (session?.user.id) {
+        try {
+          getUserAsync(session.user.id).then((user) => {
+            if (user) setCurrentUser(user);
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
 
-				<TitleSelector
-					lifeUnit={lifeUnit}
-					setLifeUnit={setLifeUnit}
-					timeUnit={timeUnit}
-					setTimeUnit={setTimeUnit}
-				/>
-				{generateGrid()}
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [getUserAsync]);
 
-				{getUserLifeHistories.isPending ? (
-					<p>Loading...</p>
-				) : getUserLifeHistories.isError ? (
-					<p>Error: {getUserLifeHistories.error.message}</p>
-				) : getUserLifeHistories.isSuccess ? (
-					<ul className="list-disc">
-						{getUserLifeHistories?.data?.map((lifeHistory) => (
-							<li key={lifeHistory.id} className="flex gap-1">
-								<p>{lifeHistory.event_text}</p>
-								<button
-									type="button"
-									onClick={() => {
-										deleteLifeHistory.mutate(lifeHistory.id);
-									}}
-									className="text-red-500"
-								>
-									Delete
-								</button>
-							</li>
-						))}
-					</ul>
-				) : null}
+  if (!currentUser) {
+    return (
+      <button type="button" onClick={signInWithGoogle}>
+        {getUserIsPending ? "Loading..." : "Iniciar sesión con Google"}
+      </button>
+    );
+  }
 
-				{/* <button
+  return (
+    <main className="flex flex-col items-center justify-center h-screen min-h-full gap-8 py-20">
+      <Header currentUser={currentUser} />
+
+      <section className="flex flex-col items-center flex-1 w-full max-w-6xl gap-4 mx-auto">
+        {createLifeHistory.isPending ? (
+          <p>Loading...</p>
+        ) : createLifeHistory.isError ? (
+          <p>Error: {createLifeHistory.error.message}</p>
+        ) : null}
+
+        {deleteLifeHistory.isPending ? (
+          <p>Loading...</p>
+        ) : deleteLifeHistory.isError ? (
+          <p>Error: {deleteLifeHistory.error.message}</p>
+        ) : null}
+
+        <TitleSelector
+          lifeUnit={lifeUnit}
+          setLifeUnit={setLifeUnit}
+          timeUnit={timeUnit}
+          setTimeUnit={setTimeUnit}
+        />
+
+        <section ref={containerRef} className="grid grid-cols-10 gap-2 mt-4">
+          {slottedItems.map((index, cell) => (
+            <div
+              key={cell}
+              className="p-2 text-center bg-gray-100 border"
+              data-swapy-slot={`cell-${index}`}
+            >
+              {index === 4 && (
+                <p className="bg-blue-500" data-swapy-item={`cell-${index}`}>
+                  content
+                </p>
+              )}
+              {cell}
+            </div>
+          ))}
+        </section>
+
+        {getUserLifeHistories.isPending ? (
+          <p>Loading...</p>
+        ) : getUserLifeHistories.isError ? (
+          <p>Error: {getUserLifeHistories.error.message}</p>
+        ) : getUserLifeHistories.isSuccess ? (
+          <ul className="list-disc">
+            {getUserLifeHistories?.data?.map((lifeHistory) => (
+              <li key={lifeHistory.id} className="flex gap-1">
+                <p>{lifeHistory.event_text}</p>
+                <button
+                  type="button"
+                  onClick={() => {
+                    deleteLifeHistory.mutate(lifeHistory.id);
+                  }}
+                  className="text-red-500"
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+
+        {/* <button
 					type="button"
-					className="border-2 border-red w-max hover:bg-slate-200 px-10 py-2"
+					className="px-10 py-2 border-2 border-red w-max hover:bg-slate-200"
 					onClick={() => {
 						setIsOpen(!isOpen);
 					}}
@@ -167,7 +205,7 @@ function App() {
 					{isOpen ? "Close" : "Create a new life history"}
 				</button> */}
 
-				{/* {!!isOpen && (
+        {/* {!!isOpen && (
 					<Modal
 						text={text}
 						setText={setText}
@@ -175,10 +213,10 @@ function App() {
 						createLifeHistory={createLifeHistory}
 					/>
 				)} */}
-			</section>
-			<Footer signOut={signOut} />
-		</main>
-	);
+      </section>
+      <Footer signOut={signOut} />
+    </main>
+  );
 }
 
 export default App;
