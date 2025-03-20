81a5234ff537cb02a7cbb8759677df772642a41c
simo El <simoghof@gmail.com>
2025-03-20 23:20:09 +0100

    feat: add support for dark mode and new color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,29 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,tsx,ts}"],
   theme: {
     extend: {
       colors: {
-        primary: "#204D3D",
-        secondary: "#B8E0D2",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)",
+        },
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)",
+        },
       },
     },
   },
